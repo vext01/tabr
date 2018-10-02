78c7aab1a9900dbbb911734e1476f146ba8cabf0
Edd Barrett <vext01@gmail.com>
2018-10-02 19:19:21 +0100

    In-place editting for existing password entries.

--- src/password.rs
+++ src/password.rs
@@ -30,13 +30,18 @@ impl EncryptedPassword {
         }
     }
 
-    pub fn to_disk(&self, path: PathBuf) -> Result<(), Box<Error>> {
+    /// Write a password to disk. If `new` is `true` then the on-disk file must not already exist.
+    pub fn to_disk(&self, path: PathBuf, new: bool) -> Result<(), Box<Error>> {
         let tml = toml::to_vec(self)?;
-        let mut pw_file = OpenOptions::new()
-                                      .write(true)
-                                      .create_new(true)
-                                      .mode(PROTECTED_MODE)
-                                      .open(path)?;
+        let mut opts = OpenOptions::new();
+        opts.write(true)
+            .mode(PROTECTED_MODE);
+
+        if new {
+            opts.create_new(true);
+        }
+
+        let mut pw_file = opts.open(path)?;
         Ok(pw_file.write_all(&tml)?)
     }
 
@@ -100,6 +105,10 @@ impl ClearPassword {
         &self.clear_text
     }
 
+    pub fn new_clear_text<A>(&mut self, clear_text: A) where A: Into<String> {
+        self.clear_text = clear_text.into();
+    }
+
     /// Consumes and encrypts the password returning an `EncryptedPassword`.
     pub fn encrypt(self, gpg: &mut GPG, encrypt_to: &Vec<String>)
                    -> Result<EncryptedPassword, Box<Error>>  {
@@ -108,6 +117,59 @@ impl ClearPassword {
                                self.email,
                                self.comment,
                                cipher_text))
+    }
+
+    pub fn username<'a>(&'a self) -> Option<&'a str> {
+        self.username.as_ref().map(|s| s.as_str())
+    }
+
+    pub fn email(&self) -> Option<&str> {
+        self.email.as_ref().map(|s| s.as_str())
+    }
+
+    pub fn comment(&self) -> Option<&str> {
+        self.comment.as_ref().map(|s| s.as_str())
+    }
+
+    pub fn new_username<A>(&mut self, username: Option<A>) where A: Into<String> {
+        self.username = username.map(|u| u.into());
+    }
+
+    pub fn new_email<A>(&mut self, email: Option<A>) where A: Into<String> {
+        self.email = email.map(|e| e.into());
+    }
+
+    pub fn new_comment<A>(&mut self, comment: Option<A>) where A: Into<String> {
+        self.comment = comment.map(|c| c.into());
+    }
+}
+
+/// A password edit description. Here a `None` means "no change".
+/// The fields have the same meaning as `ClearPassword`. Unlike `ClearPassword`, all fields are
+/// optional, since the user may choose to not change anything.
+#[derive(Debug)]
+pub struct PasswordEdit {
+    username: Option<String>,
+    email: Option<String>,
+    comment: Option<String>,
+    clear_text: Option<String>,
+}
+
+impl PasswordEdit {
+    pub fn new<A>(username: Option<A>,
+                           email: Option<A>,
+                           comment: Option<A>,
+                           clear_text: Option<A>) -> Self where A: Into<String> {
+        Self {
+            username: username.map(|a| a.into()),
+            email: email.map(|a| a.into()),
+            comment: comment.map(|a| a.into()),
+            clear_text: clear_text.map(|a| a.into()),
+        }
+    }
+
+    pub fn clear_text(&self) -> Option<&str> {
+        self.clear_text.as_ref().map(|s| s.as_str())
     }
 
     pub fn username<'a>(&'a self) -> Option<&'a str> {
