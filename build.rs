85a65f575b3427b7fa9724aac651c20f8e889234
Edd Barrett <vext01@gmail.com>
2019-02-21 11:44:38 +0000

    Remove travis config.

--- build.rs
+++ build.rs
@@ -3,4 +3,8 @@ fn main() {
     if cfg!(target_os = "openbsd") {
         println!("cargo:rustc-link-search=/usr/X11R6/lib");
     }
+
+    if cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu/");
+    }
 }
